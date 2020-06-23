1455f11251677bcbb9697f8e8d812baf32e04341
spv99 <sephoravara@gmail.com>
2020-06-23 21:13:13 +0100

    basic pie charts on all cols

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 4) get basic data from them (bar charts, percentages etc.)
 5) determine what open ended qs are / what free text responses are
 6) run sentiment analysis on free text
+
+Running in local:
+-----------------
+Local set up: https://timmyreilly.azurewebsites.net/python-pip-virtualenv-installation-on-windows/
+
+workon surveys in directory to start venv
+deactivate to exit venv
+py to run python shell
---
+++ simple-analysis.py
@@ -0,0 +1,22 @@
+import plotly.offline as py
+import plotly.graph_objects as go
+import pandas as pd
+
+df = pd.read_csv("new-york-simplified.csv")
+cols = list(df.columns.values)
+index = 0
+trace_list = [[] for _ in range(len(cols))]
+
+for col in cols:
+    options = df.iloc[:,index].dropna().unique()
+    col_value_count = df.iloc[:,index].value_counts()
+    trace = go.Pie(labels = options, values = col_value_count, title=col_value_count.name)
+    trace_list[index] = trace
+    index += 1
+
+trace_count = 0
+for trace in trace_list:
+    data = [trace_list[trace_count]]
+    fig = go.Figure(data=data)
+    py.plot(fig, filename=str(trace_count) + '-simple-pie-subplot.html')
+    trace_count += 1
